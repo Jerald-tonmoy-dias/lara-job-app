93f1c430e63748a80f03c7ae1436a31281214131
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2024-08-16 16:21:24 +0600

    add factory seeders dynamic data and relationship

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,21 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6 ">
   <div>
     <x-employer-logo />
   </div>
 
   <div class="flex-1 capitalize flex flex-col">
-    <a class="self-start text-sm capitalize text-gray-400" href="">laracasts</a>
-    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">video producer</h3>
-    <p class="text-sm text-gray-400 mt-auto">full time - from $60,000</p>
+    <a class="self-start text-sm capitalize text-gray-400" href="">{{ $job->employer->name}}</a>
+    <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{$job->title}}</h3>
+    <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
 
   </div>
 
   <div>
-    <x-tag>tag</x-tag>
-    <x-tag>tag</x-tag>
-    <x-tag>tag</x-tag>
+    @foreach($job->tags as $tag)
+    <x-tag :tag="$tag" />
+    @endforeach
   </div>
 
 
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,20 +1,20 @@
+@props(['job'])
+
 <x-panel class="flex flex-col text-center ">
 
-  <div class="self-start text-sm capitalize"> laracasts</div>
+  <div class="self-start text-sm capitalize"> {{ $job->employer->name}}</div>
   <div class="py-8  capitalize">
-    <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">video producer</h3>
-    <p class="text-sm mt-4">full time - from $60,000</p>
+    <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">{{$job->title}}</h3>
+    <p class="text-sm mt-4">{{$job->salary}}</p>
   </div>
   <div class="flex justify-between items-center mt-auto">
     <div>
-      <x-tag size="small">backend</x-tag>
-      <x-tag size="small">frontend</x-tag>
-      <x-tag size="small">api</x-tag>
-
+      @foreach($job->tags as $tag)
+      <x-tag :tag="$tag" size="small" />
+      @endforeach
 
     </div>
 
-    <!-- <img class="rounded-xl" src="http://placehold.it/42/42" alt=""> -->
     <x-employer-logo :width="42" />
   </div>
 
