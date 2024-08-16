cc227c74680ecc6efdb51e04b664679a9bb43a92
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2024-08-16 12:12:39 +0600

    initial setup

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+  <div class="self-start text-sm capitalize"> laracasts</div>
+  <div class="py-8 font-bold capitalize">
+    <h3>video producer</h3>
+    <p>full time - from $60,000</p>
+  </div>
+  <div class="flex justify-between items-center mt-auto">
+    <div>
+      <x-tag>tag</x-tag>
+      <x-tag>tag</x-tag>
+      <x-tag>tag</x-tag>
+
+
+    </div>
+
+    <img class="rounded-xl" src="http://placehold.it/42/42" alt="">
+  </div>
+
+</div>
