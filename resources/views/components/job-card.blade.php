1b0f09dc87fc2ace09f70a1e24462131530caef5
Jerald Tonmoy Dias <tonmoyjareld@gmail.com>
2024-08-16 13:21:01 +0600

    homepage

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,20 +1,21 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-panel class="flex flex-col text-center ">
 
   <div class="self-start text-sm capitalize"> laracasts</div>
-  <div class="py-8 font-bold capitalize">
-    <h3>video producer</h3>
-    <p>full time - from $60,000</p>
+  <div class="py-8  capitalize">
+    <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">video producer</h3>
+    <p class="text-sm mt-4">full time - from $60,000</p>
   </div>
   <div class="flex justify-between items-center mt-auto">
     <div>
-      <x-tag>tag</x-tag>
-      <x-tag>tag</x-tag>
-      <x-tag>tag</x-tag>
+      <x-tag size="small">backend</x-tag>
+      <x-tag size="small">frontend</x-tag>
+      <x-tag size="small">api</x-tag>
 
 
     </div>
 
-    <img class="rounded-xl" src="http://placehold.it/42/42" alt="">
+    <!-- <img class="rounded-xl" src="http://placehold.it/42/42" alt=""> -->
+    <x-employer-logo :width="42" />
   </div>
 
-</div>
+</x-panel>
